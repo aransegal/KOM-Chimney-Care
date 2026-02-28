e0e69783a6236b73e111d19bdee90f9734c1009b
Aran Segal <aran.segal@gmail.com>
2026-02-28 11:19:22 +0000

    File changes

--- src/components/home/HeroSection.jsx
+++ src/components/home/HeroSection.jsx
@@ -11,7 +11,7 @@ export default function HeroSection() {
       <div
         className="absolute inset-0 opacity-20"
         style={{
-          backgroundImage: `url("https://images.unsplash.com/photo-1504307651254-35680f356dfd?w=1920&q=80")`,
+          backgroundImage: `url("https://images.unsplash.com/photo-1558618666-fcd25c85cd64?w=1920&q=80")`,
           backgroundSize: "cover",
           backgroundPosition: "center",
         }}
