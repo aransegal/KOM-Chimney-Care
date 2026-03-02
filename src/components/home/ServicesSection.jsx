d07fa66f78c6a5a3753af32fc3e41ccacd5b8976
Aran Segal <aran.segal@gmail.com>
2026-03-02 19:32:53 +0000

    File changes

--- src/components/home/ServicesSection.jsx
+++ src/components/home/ServicesSection.jsx
@@ -47,7 +47,7 @@ export default function ServicesSection() {
           </p>
         </div>
 
-        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6">
+        <div className="grid grid-cols-1 sm:grid-cols-3 gap-6 justify-items-center">
           {services.map((service) =>
           <div
             key={service.title}
