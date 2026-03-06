fb66172c96e30e1fd70b27fdad0a91daf338a139
Aran Segal <aran.segal@gmail.com>
2026-03-06 17:12:54 +0000

    File changes

--- src/components/home/HeroSection.jsx
+++ src/components/home/HeroSection.jsx
@@ -36,8 +36,8 @@ export default function HeroSection() {
           <div className="flex flex-col sm:flex-row gap-4 mb-14">
             <Link to={createPageUrl("Booking")} className="w-full sm:w-auto">
               <Button size="lg" className="bg-green-700 hover:bg-green-800 text-white text-base px-8 h-14 shadow-lg shadow-green-900/40 w-full sm:min-w-[275px]">
+                <CalendarCheck className="mr-2 w-5 h-5" />
                 Book Installation
-                <ChevronRight className="ml-2 w-5 h-5" />
               </Button>
             </Link>
             <a href="tel:+18005551234" className="w-full sm:w-auto">
