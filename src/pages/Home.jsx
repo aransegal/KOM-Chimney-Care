818b2392599608dfee99a8494253b98dd729c2b3
Aran Segal <aran.segal@gmail.com>
2026-03-07 17:42:41 +0000

    File changes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,3 +1,4 @@
+import { useEffect } from "react";
 import HeroSection from "../components/home/HeroSection";
 import ServicesPricingSection from "../components/home/ServicesPricingSection";
 import AboutSection from "../components/home/AboutSection";
@@ -6,6 +7,15 @@ import EmergencyPopup from "../components/home/EmergencyPopup";
 import ReviewsStrip from "../components/home/ReviewsStrip";
 
 export default function Home() {
+  useEffect(() => {
+    const hash = window.location.hash.replace("#", "");
+    if (hash) {
+      setTimeout(() => {
+        document.getElementById(hash)?.scrollIntoView({ behavior: "smooth" });
+      }, 100);
+    }
+  }, []);
+
   return (
     <>
       <EmergencyPopup />
