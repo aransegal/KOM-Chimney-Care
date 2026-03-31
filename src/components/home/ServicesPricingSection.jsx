680570114175f5dd03e9f4c5d2994544fa638cdd
Aran Segal <aran.segal@gmail.com>
2026-03-31 14:17:08 +0000

    File changes

--- src/components/home/ServicesPricingSection.jsx
+++ src/components/home/ServicesPricingSection.jsx
@@ -59,7 +59,7 @@ export default function ServicesPricingSection() {
             {products.map((product) =>
             <div
               key={product.name}
-              className={`relative rounded-2xl border-2 overflow-hidden flex flex-col transition-all hover:shadow-lg border-slate-200`
+              className={`relative rounded-2xl border-2 overflow-hidden flex flex-col transition-all hover:shadow-xl hover:shadow-slate-400/50 border-slate-200`
               }>
 
                 {product.popular ?
