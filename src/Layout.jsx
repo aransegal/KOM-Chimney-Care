cf2489f9a2b9533fe800daac4b3bdf6775d833b3
Aran Segal <aran.segal@gmail.com>
2026-04-09 06:50:13 +0000

    Updated Popup Phone Number

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -89,7 +89,7 @@ export default function Layout({ children, currentPageName }) {
               {navLinks.map((link) =>
               <button
                 key={link.id}
-                onClick={() => scrollToSection(link.id)} className="text-slate-950 text-sm font-medium transition-colors hover:text-green-600">
+                onClick={() => scrollToSection(link.id)} className="text-gray-900 text-sm font-medium transition-colors hover:text-green-600">
 
 
 
