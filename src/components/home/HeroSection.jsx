f5fad96693a37f55ea4657817483e62dc06844d6
Aran Segal <aran.segal@gmail.com>
2026-05-27 19:10:42 +0000

    Manual code change

--- src/components/home/HeroSection.jsx
+++ src/components/home/HeroSection.jsx
@@ -26,7 +26,7 @@ export default function HeroSection() {
 
 
           <div className="mb-6">
-            <h1 className="text-zinc-900 font-extrabold leading-none" style={{ fontSize: "clamp(3rem, 8vw, 7rem)" }}>Water Heater</h1>
+            <h1 className="text-zinc-900 font-extrabold leading-none" style={{ fontSize: "clamp(3rem, 8vw, 7rem)" }}>Chimney Care</h1>
             <h1 className="text-green-700 font-extrabold leading-none" style={{ fontSize: "clamp(3rem, 8vw, 7rem)" }}>Experts</h1>
             <h1 className="text-zinc-900 font-extrabold leading-none" style={{ fontSize: "clamp(3rem, 8vw, 7rem)" }}>You Can Trust</h1>
           </div>
