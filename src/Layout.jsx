35ace3b303e8d433cbf01fbe6c04cd5440b8c807
Aran Segal <aran.segal@gmail.com>
2026-05-27 19:10:10 +0000

    File changes

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -80,7 +80,7 @@ export default function Layout({ children, currentPageName }) {
                 onClick={() => scrollToSection("hero")}
                 className="flex items-center">
                 <img
-                  src="https://media.base44.com/images/public/6a17301a1292b55206aaf2b1/979b5dbf2_ChatGPTImageMay27202610_05_08PM.png"
+                  src="https://media.base44.com/images/public/6a17301a1292b55206aaf2b1/be8d8058e_ChatGPT_Image_May_27__2026__10_06_47_PM-removebg-preview.png"
                   alt="KOM Water Heaters"
                   className="h-12 w-auto" />
               </button>
